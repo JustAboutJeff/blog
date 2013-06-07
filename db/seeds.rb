352174b21647a8edcc44566d117714ff35c1de2b
JEFF <justaboutjeff@gmail.com>
2013-06-06 20:56:03 -0500

    Fixes edit page submit bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# Seed data to create 5 posts with 5 tags each
+# Seed data to create 7 posts with 5 tags each
 
-5.times do
+7.times do
   post = Post.create({ author: Faker::Name.name,
                         title: Faker::Lorem.sentence((1..5).to_a.sample).chop,
-                         body: Faker::Lorem.paragraph((20..30).to_a.sample)
+                         body: Faker::Lorem.paragraph((20..60).to_a.sample)
   })
   5.times do
     tag = Tag.create({ description: Faker::Lorem.word })
