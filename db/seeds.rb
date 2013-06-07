720bad62db639e74f3f74cff6db2cb75dde9b96e
JEFF <justaboutjeff@gmail.com>
2013-06-06 20:31:40 -0500

    Additional styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 5.times do
   post = Post.create({ author: Faker::Name.name,
                         title: Faker::Lorem.sentence((1..5).to_a.sample).chop,
-                         body: Faker::Lorem.paragraph((5..9).to_a.sample)
+                         body: Faker::Lorem.paragraph((20..30).to_a.sample)
   })
   5.times do
     tag = Tag.create({ description: Faker::Lorem.word })
